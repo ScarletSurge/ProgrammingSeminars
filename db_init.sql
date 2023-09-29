e50be6b7ae09b912acbe952cd9f732cdefb859ff
Ilya Irbitskiy <scarletsurge.w@gmail.com>
2023-09-29 18:19:55 +0300

    code from 28.09.23

--- db_init.sql
+++ db_init.sql
@@ -15,3 +15,25 @@ DROP TABLE public.students
 
 ALTER TABLE public.students
     ADD COLUMN faq TEXT NOT NULL DEFAULT ('Unknown')
+
+-- INSERT SELECT UPDATE DELETE TRUNCATE
+
+INSERT INTO public.students (record_book_number, surname, name, course) VALUES
+  ('123-456', 'Ivanov', 'Ivan', 2),
+  ('123-457', 'Petrov', 'Ivan', 2),
+  ('123-458', 'Sidorov', 'Ivan', 2),
+  ('123-459', 'Bryukin', 'Maxim', 3);
+
+INSERT INTO public.students (record_book_number, surname, name, course) VALUES
+  ('666-1337', 'Tryukina', 'Kryuksima', 4);
+
+UPDATE public.students
+  SET surname = 'Hryukina'
+      -- ,name = '123'
+  WHERE surname = 'Tryukina'
+
+
+DELETE FROM public.students WHERE 1=1-- AND  OR
+
+SELECT *
+  FROM public.students s
