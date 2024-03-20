4c9b3fb9a8dba1bb6f809699a6f425d5a700f95f
scarletsurge <scarletsurge.m@gmail.com>
2024-03-20 14:36:35 +0300

    code from 20.03.2024

---
+++ RGU.dotNET.SecondTerm.Threads/Program.cs
@@ -0,0 +1,16 @@
+﻿// System.Threading.Thread
+// System.Threading.ThreadPool
+// System.Threading.ThreadStart
+// System.Threading.ParametrizedThreadStart
+// System.IAsyncResult, BeginInvoke(), EndInvoke()
+// System.Threading.Tasks.Task
+// System.Threading.Tasks.Task<T>
+// System.Threading.Tasks.ValueTask
+// System.Threading.Tasks.ValueTask<T>
+// System.Threading.CancellationToken
+// System.Threading.CancellationTokenSource
+
+// . Троельсен C# 7.0 и платформы .NET и .NET Core
+
+Parallel
+Console.WriteLine("Hello, World!");
