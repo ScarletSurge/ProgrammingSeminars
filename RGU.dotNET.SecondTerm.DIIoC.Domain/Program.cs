45abbada6a8b4cd53c78f587195334a8835338f8
scarletsurge <scarletsurge.m@gmail.com>
2024-05-22 11:27:38 +0300

    code from 22.05.24

--- RGU.dotNET.SecondTerm.DIIoC.Domain/Program.cs
+++ RGU.dotNET.SecondTerm.DIIoC.Domain/Program.cs
@@ -10,10 +10,21 @@
 
 using DryIoc;
 
+var b = new B();
+// TODO: use b
+
 var cntnr = new Container();
-cntnr.Register<A>();
-cntnr.Register<C>();
-cntnr.RegisterMapping<ISomeInterface, C>();
+cntnr.RegisterMany<A>();
+cntnr.Register<I1, D>();
+cntnr.Register<B>(Reuse.Singleton);
+//cntnr.RegisterInstance(b);
+cntnr.Register<C>(Reuse.Transient);
+//cntnr.RegisterMapping<ISomeInterface, C>();
+cntnr.RegisterMapping<ISomeInterface, B>();
+
+var resolved = cntnr.Resolve<ISomeInterface>();
+Console.WriteLine(ReferenceEquals(resolved, cntnr.Resolve<B>()));
+Console.WriteLine(ReferenceEquals(resolved, b));
 
 // Register
 // RegisterMany
@@ -23,10 +34,31 @@ cntnr.RegisterMapping<ISomeInterface, C>();
 // Resolve
 // ResolveMany
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine(ReferenceEquals(cntnr.Resolve<ISomeInterface>(), cntnr.Resolve<ISomeInterface>()));
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine(ReferenceEquals(cntnr.Resolve<ISomeInterface>(), cntnr.Resolve<ISomeInterface>()));
 
-sealed class A
+public interface I1
+{
+    void Foo();
+}
+
+public interface I2
+{
+    void Foo();
+}
+
+public sealed class D:
+    I1
+{
+    public void Foo()
+    {
+        
+    }
+}
+
+public sealed class A:
+    I1,
+    I2
 {
     private B _obj;
 
@@ -34,6 +66,16 @@ sealed class A
         B? obj)
     {
         _obj = obj ?? throw new ArgumentNullException(nameof(obj));
+    }
+
+    void I1.Foo()
+    {
+        
+    }
+
+    void I2.Foo()
+    {
+        
     }
     
     public override string ToString()
@@ -47,7 +89,8 @@ interface ISomeInterface
     void Foo();
 }
 
-sealed class B: ISomeInterface
+public sealed class B:
+    ISomeInterface
 {
     public override string ToString()
     {
@@ -60,7 +103,8 @@ sealed class B: ISomeInterface
     }
 }
 
-sealed class C: ISomeInterface
+public sealed class C:
+    ISomeInterface
 {
     public override string ToString()
     {
