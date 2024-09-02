53dd634809bded8e82e9335ba23c007bcf5587fc
scarletsurge <scarletsurge.m@gmail.com>
2024-09-02 11:02:38 +0300

    code from ?

--- RGU.dotNET.SecondTerm.DIIoC.Domain/Program.cs
+++ RGU.dotNET.SecondTerm.DIIoC.Domain/Program.cs
@@ -10,21 +10,93 @@
 
 using DryIoc;
 
-var b = new B();
-// TODO: use b
+var container = new Container();
+//var a = default(A);
+//container.RegisterInstance(a = new A(new B()));
+container.Register<B>();
+container.Register<ISomeInterface3, C>();
+container.Register<ISomeInterface1, A>(Reuse.Singleton);
+container.RegisterMapping<ISomeInterface2, ISomeInterface1>();
 
-var cntnr = new Container();
-cntnr.RegisterMany<A>();
-cntnr.Register<I1, D>();
-cntnr.Register<B>(Reuse.Singleton);
-//cntnr.RegisterInstance(b);
-cntnr.Register<C>(Reuse.Transient);
-//cntnr.RegisterMapping<ISomeInterface, C>();
-cntnr.RegisterMapping<ISomeInterface, B>();
+//
 
-var resolved = cntnr.Resolve<ISomeInterface>();
-Console.WriteLine(ReferenceEquals(resolved, cntnr.Resolve<B>()));
-Console.WriteLine(ReferenceEquals(resolved, b));
+var a1 = container.Resolve<ISomeInterface1>();
+var a2 = container.Resolve<ISomeInterface2>();
+var a3 = container.Resolve<ISomeInterface3>();
+Console.WriteLine(ReferenceEquals(a1, a2));
+a1.Foo();
+a2.Foo();
+a3.Foo();
+var x = 10;
+
+class B
+{
+    
+}
+
+class C: ISomeInterface3
+{
+    public void Foo()
+    {
+        Console.WriteLine("C.Foo() called");
+    }
+}
+
+public interface ISomeInterface1
+{
+    void Foo();
+}
+
+public interface ISomeInterface2
+{
+    void Foo();
+}
+
+public interface ISomeInterface3
+{
+    void Foo();
+}
+
+class A: ISomeInterface1, ISomeInterface2, ISomeInterface3
+{
+    private readonly B _value;
+
+    void ISomeInterface1.Foo()
+    {
+        Console.WriteLine("A.Foo() for interface1 called");
+    }
+
+    void ISomeInterface2.Foo()
+    {
+        Console.WriteLine("A.Foo() for interface2 called");
+    }
+
+    void ISomeInterface3.Foo()
+    {
+        Console.WriteLine("A.Foo() for interface3 called");
+    }
+    
+    public A(B value)
+    {
+        _value = value;
+    }
+}
+
+//var b = new B();
+//// TODO: use b
+//
+//var cntnr = new Container();
+//cntnr.RegisterMany<A>();
+//cntnr.Register<I1, D>();
+//cntnr.Register<B>(Reuse.Singleton);
+////cntnr.RegisterInstance(b);
+//cntnr.Register<C>(Reuse.Transient);
+////cntnr.RegisterMapping<ISomeInterface, C>();
+//cntnr.RegisterMapping<ISomeInterface, B>();
+
+//var resolved = cntnr.Resolve<ISomeInterface>();
+//Console.WriteLine(ReferenceEquals(resolved, cntnr.Resolve<B>()));
+//Console.WriteLine(ReferenceEquals(resolved, b));
 
 // Register
 // RegisterMany
@@ -36,83 +108,3 @@ Console.WriteLine(ReferenceEquals(resolved, b));
 
 //Console.WriteLine("Hello, World!");
 //Console.WriteLine(ReferenceEquals(cntnr.Resolve<ISomeInterface>(), cntnr.Resolve<ISomeInterface>()));
-
-public interface I1
-{
-    void Foo();
-}
-
-public interface I2
-{
-    void Foo();
-}
-
-public sealed class D:
-    I1
-{
-    public void Foo()
-    {
-        
-    }
-}
-
-public sealed class A:
-    I1,
-    I2
-{
-    private B _obj;
-
-    public A(
-        B? obj)
-    {
-        _obj = obj ?? throw new ArgumentNullException(nameof(obj));
-    }
-
-    void I1.Foo()
-    {
-        
-    }
-
-    void I2.Foo()
-    {
-        
-    }
-    
-    public override string ToString()
-    {
-        return "A instance";
-    }
-}
-
-interface ISomeInterface
-{
-    void Foo();
-}
-
-public sealed class B:
-    ISomeInterface
-{
-    public override string ToString()
-    {
-        return "B instance";
-    }
-
-    public void Foo()
-    {
-        Console.WriteLine("Foo from B");
-    }
-}
-
-public sealed class C:
-    ISomeInterface
-{
-    public override string ToString()
-    {
-        return "B instance";
-    }
-
-    public void Foo()
-    {
-        Console.WriteLine("Foo from C");
-    }
-}
