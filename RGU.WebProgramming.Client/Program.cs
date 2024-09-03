1f6d85bdd0a4e0e9b5a8e1db34c903ad97c9cf3e
scarletsurge <scarletsurge.m@gmail.com>
2024-09-03 14:04:17 +0300

    code from 02.09.2024

---
+++ RGU.WebProgramming.Client/Program.cs
@@ -0,0 +1,10 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+using RGU.WebProgramming.Grpc;
+
+var service = new MyFirstService.MyFirstServiceClient(new Channel("127.0.0.1:5055", ChannelCredentials.Insecure));
+
+await service.MyFirstRPCAsync(new Empty());
+
+Console.WriteLine("хихихих опять шарпы =)");
---
+++ RGU.WebProgramming.Server/Program.cs
@@ -0,0 +1,7 @@
+﻿using RGU.WebProgramming.Grpc;
+
+Console.WriteLine("хихихих опять шарпы =)");
+
+MyFirstModel model = new MyFirstModel();
+model.Value = 10;
+
