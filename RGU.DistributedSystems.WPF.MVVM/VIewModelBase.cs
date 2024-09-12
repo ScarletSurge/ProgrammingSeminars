5afaa62d7e108d4dab16fa2ceb440e37f0bc4183
scarletsurge <scarletsurge.m@gmail.com>
2024-09-12 18:12:39 +0300

    code from 11.09.2024

---
+++ RGU.DistributedSystems.WPF.MVVM/VIewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace RGU.DistributedSystems.WPF.MVVM;
+
+/// <summary>
+/// 
+/// </summary>
+public abstract class ViewModelBase:
+    INotifyPropertyChanged
+{
+
+    public void Foo()
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("1234"));
+    }
+    
+    #region System.ComponentModel.INotifyPropertyChanged implementation
+    
+    /// <inheritdoc cref="INotifyPropertyChanged.PropertyChanged" />
+    public event PropertyChangedEventHandler? PropertyChanged;
+    
+    #endregion
+    
+}
